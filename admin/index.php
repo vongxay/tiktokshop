108ff2a10b4d4e61b806ed3772a4bbb46e93d6cb
vongxay <vongxay689@gmail.com>
2025-12-07 20:14:03 +0700

    Fix: Remove empty line before <?php in admin/index.php causing session error

--- admin/index.php
+++ admin/index.php
@@ -1,29 +1,15 @@
-
 <?php
-    include_once('includes/header.php');
+include_once('includes/header.php');
 
-
-?>
-
-<!-- Start Container -->
-
-<?php
-    if (isset($_REQUEST['page'])) {
-        if (empty($_REQUEST['page'])) {
-            include_once('pages/home.php');
-        } else {
-            include_once('pages/' . $_REQUEST['page'] . '.php');
-        }
-    } else {
+if (isset($_REQUEST['page'])) {
+    if (empty($_REQUEST['page'])) {
         include_once('pages/home.php');
+    } else {
+        include_once('pages/' . $_REQUEST['page'] . '.php');
     }
+} else {
+    include_once('pages/home.php');
+}
 
-
-    ?>
-
-
-
-
-<?php
-    include_once('includes/footer.php');
+include_once('includes/footer.php');
 ?>
